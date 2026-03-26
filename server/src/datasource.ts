e164802eac6301713e88d640ef09a66087c3d1a1
diallosidy <diallosidymohamed99@gmail.com>
2026-03-26 17:29:05 +0100

    test CI

--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -1,8 +1,14 @@
 import { DataSource } from "typeorm";
 
+
+
 const datasource = new DataSource({
-  type: "better-sqlite3",
-  database: "./db.sqlite",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "password",
+  database: "postgres",
   entities: ["./src/entities/**/*.{js,ts}"],
   logging: true,
   synchronize: true,
